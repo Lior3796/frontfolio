715b39fba46b9d5aea4709a3edde934185314e2c
Lior3796 <liorlsa9@gmail.com>
2021-07-04 16:19:08 +0300

    "after Presentation"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+import Home from './Components/Pages/Home/Home';
+import Header from './Components/Featuers/Header/Header';
+import Footer from './Components/Featuers/Footer/Footer';
+import SideBar from './Components/Featuers/SideBar/SideBar';
+import ContainerApp from './Components/Featuers/Container/ContainerApp';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ContainerApp >  
+     
+    </ContainerApp>
     </div>
+
   );
 }
 
